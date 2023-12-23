04d82f9ae79676de7267afe48581bb09cb82eae3
Whack Rat Fan <152733236+Humbug2014@users.noreply.github.com>
2023-12-23 11:28:24 -0800

    Updated eheheheheheheheh.py
    eheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheheh

--- eheheheheheheheh.py
+++ eheheheheheheheh.py
@@ -1,12 +1,18 @@
+def fizzBuzz(num):
+    for x in range (0, num+1):
+        if x % 3 == 0 and x % 5 == 0:
+            print ("FizzBuzz")
+        if x % 3 == 0:
+            print ("Fizz")
+        if x % 5 == 0:
+            print ("Buzz")
+        else:
+            print(x)
+        
+  fizzBuzz(100)      
+        
 
 
-
-
-
-
-
-
-how do I rename it
 
 
 
